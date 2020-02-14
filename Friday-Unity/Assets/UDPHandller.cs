7b203d55641391efe13d96debfc9b8c094b8bd91
Sahaj-Bamba <sahajbamba1999@gmail.com>
2020-02-15 01:43:53 +0530

    UDP Packets tested receiving

--- Friday-Unity/Assets/UDPHandller.cs
+++ Friday-Unity/Assets/UDPHandller.cs
@@ -1,18 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
-using System.Threading;
-using UnityEngine.SceneManagement;
-
 
 public class UDPHandller : MonoBehaviour
 {
-
     public string action;
-    
-
 }
